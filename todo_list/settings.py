b1c4696117a62e26aea31ef8a5dac372c6c830bc
Natalia <natalialllepost@gmail.com>
2021-05-11 10:22:11 +0200

    database

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -69,14 +69,14 @@ WSGI_APPLICATION = 'todo_list.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        # 'ENGINE': 'django.db.backends.postgresql',
-        # 'NAME': config('DATABASE_NAME'),
-        # 'USER': config('DATABASE_USER'),
-        # 'PASSWORD': config('DATABASE_PASSWORD'),
-        # 'HOST': config('HOST'),
-        # 'PORT': '5432',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DATABASE_NAME'),
+        'USER': config('DATABASE_USER'),
+        'PASSWORD': config('DATABASE_PASSWORD'),
+        'HOST': config('HOST'),
+        'PORT': '5432',
     }
 }
 
